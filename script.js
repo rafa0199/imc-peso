1239d33202f57efc5b96423147bd93bf0f46ed99
RAFAELA DOS SANTOS PROENCA <0000110652388xsp@aluno.educacao.sp.gov.br>
2024-06-04 13:55:58 -0300

    atualizando o htmlt

---
+++ script.js
@@ -0,0 +1,5 @@
+function calcular(){
+    var fPeso = parseFloat(document.getElementById("peso"))
+    var fAltura = parseFloat(document.getElementById("altura"))
+    var fIMC = fPeso/(fAltura*fAltura)
+}
